6bb2896ae5e58891dce1b3fe070dbf316ce1729f
VincentBel <buaazqh@gmail.com>
2015-01-10 10:27:21 +0800

    建立项目结构

---
+++ src/com/vincentbel/Main.java
@@ -0,0 +1,12 @@
+package com.vincentbel;
+
+/**
+ * Author: VincentBel
+ * Create at: 2015/1/10.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
